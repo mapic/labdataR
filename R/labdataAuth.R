eaa7f90597ee222326c18b8f6bea2c23534369f0
knutole <knutole@mapic.io>
2019-12-11 11:44:02 +0100

    cleanup

--- R/labdataAuth.R
+++ R/labdataAuth.R
@@ -1,14 +1,12 @@
 #' Set up initial parameters for labdataR
 #' 
 #' @param key Access key, primary or secondary, for calling Azure Functions
-#' @param uri HTTPS URI to the Cosmos DB
-#' @param dbName String name of CosmosDB Database target
-#' @param collName String name of CosmosDB Collection target
+#' @param uri HTTPS URI to the Azure Function
 #' @return Environment variable is created for usage in all other functions
-#' @keywords environment cosmos key uri
+#' @keywords environment azure function key uri
 #' @export
 #' @examples
-#' labdataAuth("KeY123","https://yourcosmosdb.documents.azure.com","SampleDB","SampleCollection")
+#' labdataAuth("KeY123","https://your-azure-function-uri.com")
 
 
 labdataAuth <- function(key, uri) {
--- R/paramMoveToNew.R
+++ R/paramMoveToNew.R
@@ -1,15 +1,15 @@
-#' POST a Document Create to the REST API for Cosmos DB.
+#' POST a HTTP to Azure Functions, in order to clean up parameters in Labdata
 #' 
-#' @param sql.doc List of key/values to add to document. Required.
-#' @param sql.partitionkey_value The value of the partitionkey. Must also be part of sql.doc. Required. 
+#' @param param.id The ID of the parameter you wish to move. Required.
+#' @param param.code The laboratory parameter code of the parameter you wish to move. Required.
 #' @param debug.auth Logical value for getting verbose output of auth header being constructed. Defaults to false.
 #' @param debug.query Logical value for getting verbose output of HTTP response, printing all headers. Defaults to false.
 #' @param content.response Logical value to determine whether to retrieve full response or just the documents
 #' @return Prints status code of HTTP POST, and returns full HTTP response or just the content
-#' @keywords create cosmosdb post
+#' @keywords labdata parameter move new azure function post
 #' @export
 #' @examples
-#' cosmosCreate(sql.doc = list(id="uuid", code="code1"), sql.partitionkey_value = "code1")
+#' paramMoveToNew(param.id = "abc", param.code = "123", debug.query = TRUE)
 
 paramMoveToNew <- function(param.id = "", param.code = "", debug.auth = FALSE, debug.query = FALSE, content.response = TRUE) {
 
@@ -26,12 +26,7 @@ paramMoveToNew <- function(param.id = "", param.code = "", debug.auth = FALSE, d
     ms.date.string <- tolower(format(current.time, "%a, %d %b %Y %H:%M:%S %Z", tz = "GMT"))
 
     # Create POST URI for posting query to collection
-    # post.uri <- paste(envCosmosDB$uri, "/dbs/", envCosmosDB$dbName, "/colls/", envCosmosDB$collName, "/docs", sep = "")
     post.uri  <- paste0(envLabdata$uri, "?code=", envLabdata$key)
-
-    # Create the resource link and type from the environment variables
-    # res.link <- paste("dbs/", envCosmosDB$dbName, "/colls/", envCosmosDB$collName, sep = "")
-    # res.type <- "docs"
 
     # JSON body as list
     json.body <- list(
@@ -39,19 +34,6 @@ paramMoveToNew <- function(param.id = "", param.code = "", debug.auth = FALSE, d
         param_code = param.code
     )
 
-    # Set partition key (Note: must be the value, not the key!)
-    # partitionkey <- paste('["', sql.partitionkey_value, '"]', sep="")
-
-    # # Debug print
-    # if (debug.query == TRUE) {
-    #     print(paste('sql.doc:', sql.doc, sep=" "))
-    #     print(paste('partitionkey:', partitionkey, sep=" "))
-    # }
-    
-    # Generate auth header using specifications
-    # auth.header <- genHeader(verb = "POST", resource.type = res.type, resource.link = res.link, stored.time = ms.date.string, debug = debug.auth)
-    # auth.header <- genHeader(verb = "POST", stored.time = ms.date.string, debug = debug.auth)
-    
     # Geneerate POST request
     raw.response <- POST(post.uri, add_headers(.headers = c("x-ms-version" = "2017-02-22", "x-ms-date" = ms.date.string, "Content-Type" = "application/json")), body = json.body, encode = "json", verbose())
 

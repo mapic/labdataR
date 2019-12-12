2e3a8e6bcb2275e6e3f05eaa2565f768c9b49d88
knutole <knutole@mapic.io>
2019-12-12 17:08:39 +0100

    clennup

--- R/labdataMarkVerified.R
+++ R/labdataMarkVerified.R
@@ -12,7 +12,7 @@
 #' @examples
 #' labdataMarkVerified(param.id = 'abc', param.code = 'A030', param.human_verified = 'true', debug.query = TRUE)
 
-labdataMarkVerified <- function(param.id = "", param.code = "", param.human_verified = "true", debug.auth = FALSE, debug.query = FALSE, content.response = TRUE) {
+labdataMarkVerified <- function(param.id = "", param.code = "", param.human_verified = "", debug.auth = FALSE, debug.query = FALSE, content.response = TRUE) {
 
     require(digest)
     require(base64enc)
